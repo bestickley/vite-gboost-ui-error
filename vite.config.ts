eb809d6350d70390e47dc9154813ac60d8fe42f5
Ben Stickley <bestickley@gmail.com>
2022-03-09 14:28:20 -0500

    use npm

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  optimizeDeps: {
+    // I have to keep adding more and more dependencies to this...
+    // Shouldn't vite automatically pre-bundle prop-types since it's a part of gboost-ui
+    // And auto pre-bundle zen-observable since it's a part of aws-amplify?
+    include: ["aws-amplify", "@aws-amplify/ui-react", "prop-types", "zen-observable", "url"]
+  }
 })
