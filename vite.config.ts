46f0e23b13604becf46e2ca986def12189a8816b
Ben Stickley <bestickley@gmail.com>
2022-03-09 16:06:28 -0500

    does not work pnpm

--- vite.config.ts
+++ vite.config.ts
@@ -6,18 +6,17 @@ export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     // I have to keep adding more and more dependencies to this...
-    // Shouldn't vite automatically pre-bundle prop-types since it's a part of gboost-ui
+    // Shouldn't vite automatically pre-bundle prop-types since it's a part of gboost-ui?
     // And auto pre-bundle zen-observable since it's a part of aws-amplify?
     include: [
+      "prop-types",
       "aws-amplify",
       "@aws-amplify/ui-react",
-      "prop-types",
-      "zen-observable",
-      "@aws-amplify/core",
-      "@aws-amplify/auth",
-      "@aws-amplify/api-rest",
-      "@aws-amplify/pubsub",
-      "amazon-cognito-identity-js"
+      "aws-amplify > zen-observable-ts > zen-observable",
+      "aws-amplify > @aws-amplify/core > @aws-sdk/client-cloudwatch-logs > @aws-sdk/url-parser-native > url",
+      "aws-amplify > @aws-amplify/core > @aws-sdk/client-cognito-identity > @aws-sdk/url-parser-native > url",
+      "aws-amplify > @aws-amplify/core > @aws-sdk/credential-provider-cognito-identity > @aws-sdk/client-cognito-identity > @aws-sdk/url-parser-native > url",
+      "aws-amplify > @aws-sdk/url-parser-native > url",
     ],
   }
 })
